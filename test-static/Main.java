a4a6cc8c723e90d1ccc8c50ff14293002ddaf42e
David Saah <dasorange.hope@gmail.com>
2023-02-21 14:43:53 +0000

    format

--- test-static/Main.java
+++ test-static/Main.java
@@ -1,11 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        Person person1 = new Person("Naa", 300);
-        Person person2 = new Person("Yaa", 2);
+  public static void main(String[] args) {
+    Person person1 = new Person("Naa", 300);
+    Person person2 = new Person("Yaa", 2);
 
-        Person.setVotingAge(18);
-        System.out.println(Person.getVotingAge());
-        person1.display();
-        person2.display();
-    }
+    Person.setVotingAge(18);
+    System.out.println(Person.getVotingAge());
+    person1.display();
+    person2.display();
+  }
 }
