85d296ea4c2016d397fa019a4e7f6b754d79ec16
David Saah <dasorange.hope@gmail.com>
2023-08-27 02:05:38 +0000

    add templates

---
+++ Data Structures and Algorithms/lab1/Problem1.java
@@ -0,0 +1,13 @@
+/**
+ * What does the program do?
+ *
+ * @author David Saah
+ * @version 1.0
+ * @since 27-08-2023
+ *
+ */
+public class Problem1 {
+  public static void main(String[] args) {
+
+  }
+}
