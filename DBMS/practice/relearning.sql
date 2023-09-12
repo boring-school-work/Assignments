1c081c11cf36d2ed3f081c5b5534ae43737270e5
David Saah <dasorange.hope@gmail.com>
2023-09-12 21:23:23 +0000

    add dbms class practice

---
+++ DBMS/practice/relearning.sql
@@ -0,0 +1,21 @@
+create database TestDB;
+
+drop table Students;
+drop table Score;
+
+CREATE TABLE Students (
+    ID VARCHAR(45) NOT NULL,
+    Name VARCHAR(45),
+    Age FLOAT,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Score (
+    CID VARCHAR(45) NOT NULL,
+    SID INT,
+    Grade FLOAT,
+    PRIMARY KEY (CID)
+);
+
+/* cascade: all the time in exams */
+
