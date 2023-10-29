3b4e83b17534e8964dbb1264890309f60802ab82
David Saah <dasorange.hope@gmail.com>
2023-10-29 14:09:58 +0000

    add main class

---
+++ DSA/lab4/Problem 1/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Main class for the Task Management Program
+ *
+ * @author David Saah
+ * @since 2023-20-23
+ * @version 1.0
+ *
+ */
+public class Main {
+  public static void main(String[] args) {
+    // Task todo1 = new Task("T221", "I have to do", "completed", 2);
+    // Task todo2 = new Task("T222", "I have to do", "completed", 2);
+    // Task todo3 = new Task("T223", "I have to do", "completed", 2);
+    // Task todo4 = new Task("T224", "I have to do", "completed", 2);
+    // Task todo5 = new Task("T225", "I have to do", "completed", 2);
+
+    try {
+
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+}
